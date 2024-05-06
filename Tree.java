d49e4883572eed24459e27d4098e4d43e9dba156
jain-arya <149259529+jain-arya@users.noreply.github.com>
2024-05-06 22:44:46 +0530

    Tree.java

--- Tree.java
+++ Tree.java
@@ -11,7 +11,9 @@ class Main{
             System.out.println("Press 3 for preorder");
             System.out.println("Press 4 for postorder");
             System.out.println("Press 5 for level");
-            System.out.println("Press 6 to exit");
+            System.out.println("Press 6 for total no of nodes");
+            System.out.println("Press 7 for height of tree");
+            System.out.println("Press 8 to exit");
             System.out.println("Enter your choice");
             Scanner z = new Scanner(System.in);
             n = z.nextInt();
@@ -28,11 +30,14 @@ class Main{
                 case 3: preorder(root); break;
                 case 4: postorder(root); break;
                 case 5: level(root); break;
-                case 6: break;
+                case 6: System.out.println("Total no of nodes = " + count(root));
+                        break;
+                case 7: System.out.println("Height of tree is = " + height(root)); break;
+                case 8: break;
                 default : System.out.println("Invalid no!!!");
             }
         }
-        while(n != 6);
+        while(n != 8);
     }
     static  Node create(Node p , int no){
         if(p == null){
@@ -108,6 +113,25 @@ class Main{
             }
         }
     }
+    
+    static int count(Node p){
+        if(p == null){
+            return 0;
+        }
+        int l = count(p.ladd);
+        int r = count(p.radd);
+        return l+r+1;
+    }
+    
+    static int height(Node p){
+        if(p == null){
+            return 0;
+        }
+        int l = height(p.ladd);
+        int r = height(p.radd);
+        int ans = Math.max(l,r);
+        return ans+1;
+    }
 }
 
 class Node{
@@ -116,7 +140,6 @@ class Node{
     Node radd;
 }
     
-
 
 
 
